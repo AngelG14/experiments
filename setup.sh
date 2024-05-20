f31a0d316ae46dc4f1cd0db17eba34f2961bc415
AngelG14 <angeldagosa@gmail.com>
2024-05-20 17:32:01 -0500

    now naiveautoml is installed from repository

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
-repo_url="https://github.com/fmohr/naiveautoml.git"
-branch="55-create-support-for-custom-stoppers-for-early-discarding"
-git clone "$repo_url" --branch "$branch"
 git clone https://github.com/automl/lcpfn.git
 python3 -m venv .env
 source .env/bin/activate
+pip install git+https://github.com/fmohr/naiveautoml.git@55-create-support-for-custom-stoppers-for-early-discarding#subdirectory=python
 pip install -r ./naiveautoml/python/requirements.txt
 pip install -r ./experiment/requirements.txt
 pip install -e ./naiveautoml/python/
