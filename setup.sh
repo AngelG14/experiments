1ce41270aff5cabdbdc93b582a2f8e7020ca2c64
AngelG14 <angeldagosa@gmail.com>
2024-05-22 19:10:37 -0500

    fixed pip install from remote repository

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 git clone https://github.com/automl/lcpfn.git
 python3 -m venv .env
 source .env/bin/activate
-pip install git+https://github.com/fmohr/naiveautoml.git@55-create-support-for-custom-stoppers-for-early-discarding#subdirectory=python
-pip install -r ./naiveautoml/python/requirements.txt
+pip install -e 'git+https://github.com/fmohr/naiveautoml.git@55-create-support-for-custom-stoppers-for-early-discarding#egg=naiveautoml&subdirectory=python'
+pip install lccv
 pip install -r ./experiment/requirements.txt
-pip install -e ./naiveautoml/python/
 mv pyproject.toml ./lcpfn/
 pip install -e ./lcpfn/
 python experiment/config_experiment.py
